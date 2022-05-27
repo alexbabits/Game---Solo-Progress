0c166acd4a493314eaa410898affc4ad8f822bc1
Alex Babits <alexbabits@gmail.com>
2022-05-26 23:42:53 -0400

    stamina bar UI working, no chamfer glitchy

--- js/Player.js
+++ js/Player.js
@@ -12,7 +12,7 @@ export default class Player extends MatterEntity {
         //x and y position based on game configs and adjusted for zoom: EX: ((height - (height/zoom))/2. ((640 - (640/1.4))/2 = 91.43 becomes the new (0,0).
         this.hp = new HealthBar(this.scene, 100, 100, this.health, this.maxHealth);
         //atempt to add in the stamina bar.
-        this.energy = new StaminaBar(this.scene, 218, 100, this.stamina, this.maxStamina);
+        this.energy = new StaminaBar(this.scene, 235, 117, this.stamina, this.maxStamina);
 
         this.attackFlag = false;
         this.walkingSwitch = false;
--- js/StaminaBar.js
+++ js/StaminaBar.js
@@ -14,7 +14,7 @@ export default class StaminaBar extends Phaser.Scene {
         this.staminaDenominator = maxStamina;
         this.size = {
             width: 200,
-            height: 11
+            height: 9
         }
         this.pixelPerStamina = this.size.width/this.staminaDenominator;
 
@@ -40,39 +40,27 @@ export default class StaminaBar extends Phaser.Scene {
         this.text.setText(`${this.staminaValue}/${this.staminaDenominator}`);
 
         const { width, height } = this.size;
-        const margin = 2;
-        const offset = 15;
         const chamfer = 4;
-        const staminaWidth = (this.staminaValue * this.pixelPerStamina);
+        const staminaWidth = this.staminaValue * this.pixelPerStamina;
         
         this.bar.fillStyle(0xFFFFFF);
-        this.bar.fillRoundedRect(x + offset + margin, y + offset + margin, width - margin, height - margin, chamfer);
-
-        if(this.size.width <= this.pixelPerStamina * this.staminaValue){
-            this.size.width = 200
-        }
+        this.bar.fillRoundedRect(x, y, width, height, 0);
 
         if(staminaWidth <= this.size.width && staminaWidth > this.size.width/1.333){
-            this.bar.fillStyle(0x00b300);
+            this.bar.fillStyle(0x00FF00);
         } else if (staminaWidth <= this.size.width/1.333 && staminaWidth > this.size.width/2) {
-            this.bar.fillStyle(0xe6e600);
+            this.bar.fillStyle(0xFFFF00);
         } else if (staminaWidth <= this.size.width/2 && staminaWidth >= this.size.width/4) {
-            this.bar.fillStyle(0xff9000);
-        } else if(staminaWidth <= 0){ //fixes the problem with numbers near zero not filling correctly.
-            this.bar.fillStyle(0xFFFFFF);
-            this.bar.fillRoundedRect(x + offset + margin, y + offset + margin, 0, height - margin, chamfer);
+            this.bar.fillStyle(0xFFa500);
         } else {
             this.bar.fillStyle(0xFF0000);
         }
 
         //almost fixed graphics for now, think it has to do with stamina going above max or below min.
-            if(this.staminaValue >= this.staminaDenominator){
-                this.bar.fillRoundedRect(x + offset + margin, y + offset + margin, staminaWidth - margin, height - margin, chamfer);
-            } else {
-                this.bar.fillRoundedRect(x + offset + margin, y + offset + margin, staminaWidth + margin, height - margin, chamfer);
-            }
 
-        //if the physical length of the bar exceeds 200, then set it back to 200.
+        if(this.staminaValue > 0){
+            this.bar.fillRoundedRect(x, y, staminaWidth, height, 0);
+        }
 
     };
 
